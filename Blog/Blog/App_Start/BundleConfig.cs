6ff61231145b5d8a76d2bc5bab5dc12e66fc3904
Zlatyo Uzunov <zluzunov@gmail.com>
2016-12-14 02:55:51 +0200

    Remove junk files Add design giles - css, js Add design css and js to Layout and to BundleConfig

--- Blog/Blog/App_Start/BundleConfig.cs
+++ Blog/Blog/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace Blog
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-2.1.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,9 +23,22 @@ namespace Blog
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/fancybox").Include(
+                      "~/Scripts/fancybox/jquery.fancybox.pack.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/responsive-nav").Include(
+                      "~/Scripts/responsive-nav/responsive-nav.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/common.css",
+                      "~/Content/forms.css",
+                      "~/Content/gallery.css",
+                      "~/Content/jquery.fancybox.css",
+                      "~/Content/jquery.media.css",
+                      "~/Content/jquery.navigation.css",
+                      "~/Content/jquery.responsive-nav.css",
+                      "~/Content/jquery.typography.css"));
         }
     }
 }
