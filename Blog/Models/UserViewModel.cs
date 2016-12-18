95975a2c89e6aee4aced4e549892adc243bc1854
Pavlin Nedelchev <minimaanima@gmail.com>
2016-12-18 21:47:31 +0200

    dfdfdsfdsfsf

---
+++ Blog/Models/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class UserViewModel
+    {
+        public string Username { get; set; }
+        public string Roles { get; set; }
+    }
+
+    public class GroupedUserViewModel
+    {
+        public List<UserViewModel> Users { get; set; }
+        public List<UserViewModel> Admins { get; set; }
+    }
+
+}
